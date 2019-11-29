16d898ea1018289518ebce3676ee1848c98d58a4
SkaziWeb <skazi.web@yandex.ru>
2019-11-30 00:02:00 +0700

    Подключил к апи

--- somebot/dist/common.js
+++ somebot/dist/common.js
@@ -1,3 +1,4 @@
+var api = "";
 function getTimeZone() {
     var format, timezone;
     if (typeof Intl === "undefined" || typeof Intl.DateTimeFormat === "undefined") {
@@ -16,43 +17,196 @@ function getTimeZone() {
         return timezone;
     }
 }
+function dateToUTCStr(d) {
+    d = new Date(d);
+    var date = d.getUTCFullYear() + '-' + zerofix(d.getUTCMonth() + 1) + '-' + zerofix(d.getUTCDate());
+    var time = zerofix(d.getUTCHours()) + ':' + zerofix(d.getUTCMinutes()) + ':' + zerofix(d.getUTCSeconds());
+    return date + 'T' + time + '.000Z';
+}
 
-function availableDays(){
+function zerofix(val) {
+    return (val < 10 ? '0' : '') + val;
+}
+
+function isYoungChildYear(date) {
+    if (!date) {
+        return false;
+    }
+    var d = new Date(date.getFullYear() + youngAge + 1, date.getMonth(), date.getDate(), 0, 0, 0);
+    return d.getTime() >= new Date().getTime();
+}
+
+function isYoungAndFemaleChild(date, gender) {
+    return isYoungChildYear(date) && gender === 'female';
+}
+
+function isPreschoolChildYear(date) {
+    if (!date) {
+        return false;
+    }
+    var d = new Date(date.getFullYear() + preschoolAge + 1, date.getMonth(), date.getDate(), 0, 0, 0);
+    return d.getTime() >= new Date().getTime();
+}
+
+function makeTrial() {
+    var data = {
+        "email": USER_DATA.email,
+        "name": USER_DATA.parentName,
+        "phone": USER_DATA.phone,
+        "promo_code": USER_DATA.promo_code,
+        //"students": students,
+        "timezone": getTimeZone(),
+        "region_code": 'RU',
+        "visitor_id": USER_DATA.visitor_id,
+        'utm_data': utm_data || undefined,
+        "referral_code": USER_DATA.promo_code || (utm_data && utm_data.ref)
+    };
     return $.ajax({
-        url: api + 'teachers_schedule/days',
-        data: {
-            trial_id: data && data.id ? data.id : undefined,
-            is_busy: false,
-            accept_new_students: true,
-            trial_rank_over_zero: true,
-            region_code: regionCode || undefined,
-            gender: isYoungAndFemaleChild($('#choose-first-child-year').data('date'), $('input[name=firstChildGender]')) ? 'female' : undefined,
-            teacher_l0_certified: isPreschoolChildYear($('#choose-first-child-year').data('date')) ? true : undefined,
-            //teacher_is_near_native: $('#cbxOnlyNative').is(':checked') ? false : undefined,
-            teacher_is_near_native: teacherSelector ? $('input.thx-teacher-type:checked').val() !== 'native' : defaultTeacherType !== 'native',
-            start_time__gte: from,
-            start_time__lte: to,
-            timestamp: new Date().getTime()
-        }
+        url: api + 'trial_requests',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(data)
     });
 }
 
-var now = (new Date());
+function availableDays(){
+    var from = dateToUTCStr(new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, now.getHours() + 1, now.getMinutes(), now.getSeconds())),
+        to = dateToUTCStr(new Date(now.getFullYear(), now.getMonth(), now.getDate() + 30, 0, 0, 0));
+    var data = {
+        trial_id: data && data.id ? data.id : undefined,
+        is_busy: false,
+        accept_new_students: true,
+        trial_rank_over_zero: true,
+        region_code: 'RU',
+        gender: isYoungAndFemaleChild(new Date(USER_DATA.bithdate), USER_DATA.gender) ? 'female' : undefined,
+        teacher_l0_certified: isPreschoolChildYear(new Date(USER_DATA.bithdate)) ? true : undefined,
+        teacher_is_near_native: USER_DATA.teacherType === 'native' ? true : false,
+        start_time__gte: from,
+        start_time__lte: to,
+        timestamp: new Date().getTime()
+    };
+    return $.ajax({
+        url: api + 'teachers_schedule/days',
+        data: data
+    });
+};
+function availableSlot() {
+    var date = new Date(USER_DATA.trialLessonDate.split('.').reverse().join(' '));
+    var data = {
+        trial_id: data && data.id ? data.id : undefined,
+        order_by: 'start_time',
+        is_busy: false,
+        accept_new_students: true,
+        trial_rank_over_zero: true,
+        region_code: USER_DATA.regionCode || undefined,
+        gender: isYoungAndFemaleChild(new Date(USER_DATA.bithdate), USER_DATA.gender) ? 'female' : undefined,
+        teacher_l0_certified: isPreschoolChildYear(new Date(USER_DATA.bithdate)) ? true : undefined,
+        //teacher_is_near_native: $('#cbxOnlyNative').is(':checked') ? false : undefined,
+        teacher_is_near_native: USER_DATA.teacherType === 'native' ? true : false,
+        start_time__gte: dateToUTCStr(Math.max(date.getTime(), new Date().getTime() + 1000 * 60 * 60 * 24)),
+        start_time__lte: dateToUTCStr(date.getTime() + 1000 * 60 * 60 * 24),
+        timestamp: new Date().getTime()
+    };
+    return $.ajax({
+        url: api + 'https://api-qa.novakidschool.com/api/0/teachers_schedule/slots',
+        data: data
+    });
+};
+
+function splitSlots(data) {
+    BOT_CONFIG[EveningState].nextState.answers= [];
+    BOT_CONFIG[DayState].nextState.answers = [];
+    BOT_CONFIG[MorningState].nextState.answers = [];
+    return new Promise(function(resolve, reject){
+        
+        var slots = data.map(function (e) {
+            var time = e.start_time.substr(11, 5);
+            var teacher = e.teacher_id;
+            return {
+                text: time,
+                value: "" + time + ":" + teacher
+            };
+        });
+        for (var idx in slots) {
+            if (parseInt(slots[idx].text) < 13) {
+                BOT_CONFIG[MorningState].nextState.answers.push(slots[idx]);
+            } else if (parseInt(slots[idx].text) >= 13 && parseInt(slots[idx].text) < 18) {
+                BOT_CONFIG[DayState].nextState.answers.push(slots[idx]);
+            } else {
+                BOT_CONFIG[EveningState].nextState.answers.push(slots[idx]);
+            }
+        }
+        
+        BOT_CONFIG[EveningState].nextState.answers.push(stepBack);
+        BOT_CONFIG[DayState].nextState.answers.push(stepBack);
+        BOT_CONFIG[MorningState].nextState.answers.push(stepBack);
+        resolve(BOT_CONFIG);
+    });
+}
+
+function getTimeIntervalAnswers(answers){
+    return new Promise(function (resolve, reject) {
+        if (BOT_CONFIG[MorningState].nextState.answers.length > 1) {
+            answers.push({
+                text : 'Утро с 09:00 до 12:30',
+                value: MorningState
+            });
+        } 
+        if (BOT_CONFIG[DayState].nextState.answers.length > 1) {
+            answers.push({
+                text : 'День с 13:00 до 17:30',
+                value: DayState
+            });
+        } 
+        if (BOT_CONFIG[EveningState].nextState.answers.length > 1) {
+            answers.push({
+                text : 'Вечер с 18:00 до 20:00',
+                value: EveningState
+            });
+        } 
+        answers.push(stepBackDay);
+        resolve(answers);
+    });
+}
+
+var now = new Date();
 var convForm;
+var preschoolAge = 5;
+var youngAge = 8;
 var USER_DATA = {
     email: '',
     name: '',
+    age:'',
+    bithdate: '',
     phone: '',
     gender: '',
     promo_code: '',
     //students: students,
     timezone: getTimeZone(),
-    region_code: '',
+    region_code: 'RU',
     visitor_id: '',
     utm_data: '',
-    referral_code: ''
+    referral_code: '',
+    parentName:'',
+    phone:'',
+
 };
-var TEST_CONFIG = {
+
+var MorningState = 'trialLessonTimeMorning';
+var DayState = 'trialLessonTimeDay';
+var EveningState = 'trialLessonTimeEvening';
+var stepBack = {
+    text: 'Выбрать другой интервал',
+    value: 'stepBack'
+};
+var stepBackDay = {
+    text: 'Выбрать другой день',
+    value: 'stepBack'
+};
+
+var BOT_CONFIG = {
     name: {
         pattern: /[a-zA-ZА-Яа-яёй]{3,}/,
         nextStateName: 'gender'
@@ -96,81 +250,47 @@ var TEST_CONFIG = {
             type: 'select',
             name: 'trialLessonDate',
             questions: ['Выберите дату и время пробного урока. Слоты доступны с 9 до 20.00 по мск времени.'],
-            //TODO: загружаем даты с сервера
-            answers: [
-                {
-                    text: 'Сегодня',
-                    value: '1'
-                },
-                {
-                    text: 'Завтра',
-                    value: '2'
-                },
-                {
-                    text: 'Послезавтра',
-                    value: '3'
-                }
-            ]
+            answers: []
         }
     },
     trialLessonInterval: {
-        nextStateName: 'trialLessonTime',
+        nextStateName: 'dynamic',
         prevStateName: 'trialLessonDate',
         nextState: {
             type: 'select',
             name: 'trialLessonInterval',
             questions: ['Выберите удобный для Вас интервал.'],
-            //TODO: загружаем даты с сервера
-            answers: [
-                {
-                    text: 'Утро с 9:00 до 12:30',
-                    value: '1'
-                },
-                {
-                    text: 'День с 13:00 до 17:30',
-                    value: '2'
-                },
-                {
-                    text: 'Вечер с 18:00 до 20:00',
-                    value: '3'
-                },
-                {
-                    text: 'Выбрать другой день',
-                    value: 'stepBack'
-                },
-                {
-                    text: 'END',
-                    value: 'END'
-                }
-            ]
+            answers: []
         }
     },
-    trialLessonTime: {
+    trialLessonTimeMorning: {
         nextStateName: 'teacherType',
         prevStateName: 'trialLessonInterval',
         nextState: {
             type: 'select',
-            name: 'trialLessonTime',
-            questions: ['Выберите дату и время пробного урока. Слоты доступны с 9 до 20.00 по мск времени.'],
-            //TODO: загружаем время с сервера
-            answers: [
-                {
-                    text: '17:30',
-                    value: '1'
-                },
-                {
-                    text: '18:00',
-                    value: '2'
-                },
-                {
-                    text: '20:00',
-                    value: '3'
-                },
-                {
-                    text: 'Выбрать интервал',
-                    value: 'stepBack'
-                }
-            ]
+            name: 'trialLessonTimeMorning',
+            questions: ['Утренние слоты'],
+            answers: []
+        }
+    },
+    trialLessonTimeDay: {
+        nextStateName: 'teacherType',
+        prevStateName: 'trialLessonInterval',
+        nextState: {
+            type: 'select',
+            name: 'trialLessonTimeDay',
+            questions: ['Дневные слоты'],
+            answers: []
+        }
+    },
+    trialLessonTimeEvening: {
+        nextStateName: 'teacherType',
+        prevStateName: 'trialLessonInterval',
+        nextState: {
+            type: 'select',
+            name: 'trialLessonTimeEvening',
+            questions: ['Вечерние слоты'],
+            answers: []
         }
     },
     teacherType: {
@@ -179,7 +299,6 @@ var TEST_CONFIG = {
             type: 'select',
             name: 'teacherType',
             questions: ['Стандарт – занятие с преподавателем, свободно владеющим английским языком, от 490 р. / урок. Премиум– занятия с носителем английского языка, от 890 р. / урок'],
-            //TODO: загружаем время с сервера
             answers: [
                 {
                     text: 'Премиум',
@@ -252,7 +371,7 @@ var TEST_CONFIG = {
         }
     }
 };
-const openChat = function openChat(){
+var openChat = function openChat(){
     if (convForm) {
         return false;
     }
@@ -264,6 +383,7 @@ const openChat = function openChat(){
                 userInput.datepicker({
                     position: 'top right',
                     view: 'years',
+                    autoClose: true,
                     minDate: params.minDate,
                     maxDate: params.maxDate
                 });
@@ -278,7 +398,9 @@ const openChat = function openChat(){
         eventList: {
             onInputSubmit: function (convState, ready) {
                 var key = convState.current.input.name || convState.current.input.selected;
-                var nextStateName = TEST_CONFIG[key] && TEST_CONFIG[key].nextStateName ? TEST_CONFIG[key].nextStateName : null;
+                var nextStateName = BOT_CONFIG[key] && BOT_CONFIG[key].nextStateName ? BOT_CONFIG[key].nextStateName : null;
+                var answer, value;
+                var answers = [];
                 // TODO: Нужен ли тест валидации если это делает маска ввода?
                 if (convState.current.answer.value === 'end' || nextStateName === 'end') {
                     convState.current.next = false;
@@ -288,19 +410,74 @@ const openChat = function openChat(){
                         answer = convState.current.answer.join(', ');
                     } else {
                         answer = convState.current.answer.text;
+                        value = convState.current.answer.value;
                     }
+
                     if (convState.current.answer.value === 'stepBack') {
-                        nextStateName = TEST_CONFIG[key].prevStateName;
+                        nextStateName = BOT_CONFIG[key].prevStateName;
                     }
-                    if (TEST_CONFIG[key] && TEST_CONFIG[key].nextState && TEST_CONFIG[key].nextState.type === 'date') {
+                    if (nextStateName === 'dynamic') {
+                        nextStateName = value;
+                    }
+                    if (BOT_CONFIG[key] && BOT_CONFIG[key].nextState && BOT_CONFIG[key].nextState.type === 'date') {
                         userInput.datepicker().data('datepicker').destroy();
                         userInput.val('');
                     }
-                    USER_DATA[key] = answer;
+                    if (key === 'gender' || key === 'teacherType') {
+                        USER_DATA[key] = value;
+                    } else if (key === 'age') {
+                        var bithdate = new Date(answer.split('.').reverse().join(' '));
+                        USER_DATA.bithdate = answer.split('.').reverse().join(' ');
+                        USER_DATA[key] = (new Date().getFullYear() - bithdate.getFullYear());
+                    } else {
+                        USER_DATA[key] = answer;
+                    }
                     if (nextStateName) {
-                        changeUserInput(TEST_CONFIG[nextStateName].nextState);
-                        convState.current.next = convState.newState(TEST_CONFIG[nextStateName].nextState);
-                        setTimeout(ready, Math.random() * 500 + 100);
+                        changeUserInput(BOT_CONFIG[nextStateName].nextState);
+                        if (nextStateName === 'trialLessonDate') {
+                            availableDays()
+                            .then(function (rsp) {
+                                if (rsp && rsp.days.length > 0) {
+                                    answers = rsp.days.map(function (e, idx) {
+                                        return {
+                                            text: e.substr(0, 10).split('-').reverse().join('.'),
+                                            value: "" + idx + ""
+                                        };
+                                    });
+                                } else {
+                                    nextStateName = 'notSlot';
+                                }
+                                var nextState = {
+                                    type: 'select',
+                                    name: 'trialLessonDate',
+                                    questions: ['Выберите дату и время пробного урока. Слоты доступны с 9 до 20.00 по мск времени.'],
+                                    answers: answers
+                                };
+                                convState.current.next = convState.newState(nextState);
+                            });
+                        } else if (nextStateName === 'trialLessonInterval') {
+                            availableSlot()
+                            .then(function(rsp){
+                                if (rsp && rsp._embedded && rsp._embedded.slots && rsp._embedded.slots.length > 0) {
+                                    return splitSlots(rsp._embedded.slots);
+                                }
+                            })
+                            .then(function (rsp) {
+                                return getTimeIntervalAnswers(answers);
+                            })
+                            .then(function (rsp) {
+                                var nextState = {
+                                    type: 'select',
+                                    name: 'trialLessonInterval',
+                                    questions: rsp.length > 1 ? ['Выберите удобный для Вас интервал.'] : ['К сожалению в данный день все места уже заняты.'],
+                                    answers: rsp
+                                };
+                                convState.current.next = convState.newState(nextState);
+                            });
+                        } else {
+                            convState.current.next = convState.newState(BOT_CONFIG[nextStateName].nextState);
+                        }
+                        setTimeout(ready, Math.random() * 1000 + 100);
                     }
                 }
             }
@@ -311,7 +488,9 @@ const openChat = function openChat(){
 };
 
 const closeChat = function () {
-    convForm.destroy();
-    convForm = null;
+    if (convForm) {
+        convForm.destroy();
+        convForm = null;
+    }
     $('#demo').addClass('hidden');
 };
