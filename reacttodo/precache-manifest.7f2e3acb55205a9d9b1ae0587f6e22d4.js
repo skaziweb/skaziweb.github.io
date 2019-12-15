f15b9acfea386da5b8b18ec6c57f1e527f055468
Aleksey Kuzmin <skazi.web@yandex.ru>
2019-12-15 20:30:36 +0700

    Add simple react  todo app

---
+++ reacttodo/precache-manifest.7f2e3acb55205a9d9b1ae0587f6e22d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455a2118f4027ca29c996a668a0e9075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6185503a8eb6101d952d",
+    "url": "/static/css/main.ec0a4f03.chunk.css"
+  },
+  {
+    "revision": "2e53c95978a809ba2a53",
+    "url": "/static/js/2.124713bb.chunk.js"
+  },
+  {
+    "revision": "6185503a8eb6101d952d",
+    "url": "/static/js/main.9f608e3a.chunk.js"
+  },
+  {
+    "revision": "2128b6d0c34069236117",
+    "url": "/static/js/runtime-main.3f7648bc.js"
+  }
+]);
