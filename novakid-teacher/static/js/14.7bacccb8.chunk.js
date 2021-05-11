42ba36d96aecbca3fa70ea3d27c9629fefcead7d
Aleksey Kuzmin <aleksey@novakidschool.com>
2021-05-12 01:22:51 +0700

    upd

---
+++ novakid-teacher/static/js/14.7bacccb8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnovakid-teacher"]=this["webpackJsonpnovakid-teacher"]||[]).push([[14],{371:function(t,a,n){"use strict";n.d(a,"a",(function(){return e})),n.d(a,"b",(function(){return c}));var r=n(0);function e(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"}}]})(t)}function c(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"}}]})(t)}}}]);
+//# sourceMappingURL=14.7bacccb8.chunk.js.map
--- teacher/static/js/14.a4e8b10a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnovakid-teacher"]=this["webpackJsonpnovakid-teacher"]||[]).push([[14],{364:function(t,a,n){"use strict";n.d(a,"a",(function(){return e})),n.d(a,"b",(function(){return c}));var r=n(0);function e(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"}}]})(t)}function c(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"}}]})(t)}}}]);
-//# sourceMappingURL=14.a4e8b10a.chunk.js.map
